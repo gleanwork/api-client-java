b452171f92728e62bc7451f96da6bfe5f7777170
Chris Freeman <chris.freeman@glean.com>
2026-03-04 10:01:16 -0700

    fix: use fully qualified https:// URLs in documentation and Javadoc

--- src/main/java/com/glean/api_client/glean_api_client/hooks/GleanBuilder.java
+++ src/main/java/com/glean/api_client/glean_api_client/hooks/GleanBuilder.java
@@ -20,7 +20,7 @@ import java.util.Optional;
  * <pre>{@code
  * Glean glean = GleanBuilder.create()
  *         .apiToken("your-api-token")
- *         .serverURL("mycompany-be.glean.com")
+ *         .serverURL("https://mycompany-be.glean.com")
  *         .excludeDeprecatedAfter("2026-10-15")
  *         .includeExperimental(true)
  *         .build();
